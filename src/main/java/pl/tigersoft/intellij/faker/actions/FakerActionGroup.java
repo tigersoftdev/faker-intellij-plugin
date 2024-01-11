ffe651192859c9aa7e7fd7aa915fc7a5c538a5a8
Erik Pragt <erik.pragt@gmail.com>
2024-01-11 21:27:11 +1100

    Upgrade of Datafaker to the latest version. Also migrated project to Java 17, and updated all dependencies.

--- src/main/java/pl/tigersoft/intellij/faker/actions/FakerActionGroup.java
+++ src/main/java/pl/tigersoft/intellij/faker/actions/FakerActionGroup.java
@@ -10,6 +10,7 @@ import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -18,6 +19,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
+
 import net.datafaker.Faker;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
@@ -26,105 +28,107 @@ import pl.tigersoft.intellij.faker.FakerNotifier;
 
 public class FakerActionGroup extends ActionGroup {
 
-	private static final Logger log = Logger.getInstance(FakerActionGroup.class);
+    private static final Logger log = Logger.getInstance(FakerActionGroup.class);
 
-	private static final List<String> excludedClasses = Arrays.asList("Options", "Number", "CreditCardType", "Faker");
+    private static final List<String> excludedClasses = Arrays.asList("Number", "CreditCardType", "Faker", "Get");
+    private static final List<String> excludedMethods = Arrays.asList("wait", "notify", "notifyAll", "toString", "getClass", "hashCode", "getFaker");
 
-	private static final Comparator<Method> methodComparator = (method1, method2) ->
-    method1.getName().compareToIgnoreCase(method2.getName());
+    private static final Comparator<Method> methodComparator = (method1, method2) ->
+            method1.getName().compareToIgnoreCase(method2.getName());
 
-	@NotNull
-	@Override
-	public AnAction[] getChildren(@Nullable AnActionEvent event) {
-		Faker faker = Faker.instance();
-		return Arrays.stream(faker.getClass().getDeclaredMethods())
-			.filter(categoryFilter())
-			.sorted(methodComparator)
-			.map(method -> buildActionGroup(method, faker))
-			.toArray(AnAction[]::new);
-	}
-
-	@NotNull
-	private ActionGroup buildActionGroup(Method method, Faker faker) {
-		return new ActionGroup(prettyName(method.getName()), true) {
-
-      @NotNull
-      @Override
-      public AnAction[] getChildren(@Nullable AnActionEvent event) {
-        return invokeMethod(method, faker, event)
-          .map(object -> Arrays.stream(object.getClass().getDeclaredMethods())
-            .filter(subcategoryFilter())
-            .sorted(methodComparator)
-            .map(method -> getAction(method, object))
-            .toArray(AnAction[]::new))
-          .orElse(new AnAction[0]);
-      }
-    };
-  }
-
-	@NotNull
-	private AnAction getAction(Method method, Object object) {
-		return new AnAction(prettyName(method.getName())) {
-
-      @Override
-      public void actionPerformed(@NotNull AnActionEvent event) {
-        invokeMethod(method, object, event).ifPresent(fake -> insertFake(event, fake));
-      }
-
-			@Override
-			public void update(@NotNull final AnActionEvent event) {
-				final Project project = event.getProject();
-				final Editor editor = event.getData(CommonDataKeys.EDITOR);
-
-				event.getPresentation().setEnabledAndVisible(project != null && editor != null);
-			}
-		};
-	}
-
-	private void insertFake(AnActionEvent event, Object fake) {
-		String text = String.valueOf(fake);
-		final Editor editor = event.getRequiredData(CommonDataKeys.EDITOR);
-		final Project project = event.getRequiredData(CommonDataKeys.PROJECT);
-		final Document document = editor.getDocument();
-
-		Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
-		int start = primaryCaret.getSelectionStart();
-		int end = primaryCaret.getSelectionEnd();
-
-		WriteCommandAction.runWriteCommandAction(project, () -> document.replaceString(start, end, text));
-		primaryCaret.moveToOffset(start + text.length());
-		primaryCaret.removeSelection();
-	}
-
-  private static Optional<Object> invokeMethod(Method method, Object object, AnActionEvent event) {
-    if (method == null || object == null) {
-      return Optional.empty();
+    @NotNull
+    @Override
+    public AnAction[] getChildren(@Nullable AnActionEvent event) {
+        Faker faker = new Faker();
+        return Arrays.stream(faker.getClass().getMethods())
+                .filter(categoryFilter())
+                .sorted(methodComparator)
+                .map(method -> buildActionGroup(method, faker))
+                .toArray(AnAction[]::new);
     }
-    final Project project = event.getRequiredData(CommonDataKeys.PROJECT);
-    try {
-      return Optional.ofNullable(method.invoke(object));
-    } catch (IllegalAccessException | InvocationTargetException e) {
-      String msg = String.format("Unable to invoke method '%s()' on object '%s'",
-        method.getName(), object.getClass().getSimpleName());
-      log.error(msg);
-      FakerNotifier.error(project, msg);
+
+    @NotNull
+    private ActionGroup buildActionGroup(Method method, Faker faker) {
+        return new ActionGroup(prettyName(method.getName()), true) {
+
+            @NotNull
+            @Override
+            public AnAction[] getChildren(@Nullable AnActionEvent event) {
+                return invokeMethod(method, faker, event)
+                        .map(object -> Arrays.stream(object.getClass().getMethods())
+                                .filter(subcategoryFilter())
+                                .sorted(methodComparator)
+                                .map(method -> getAction(method, object))
+                                .toArray(AnAction[]::new))
+                        .orElse(new AnAction[0]);
+            }
+        };
     }
-    return Optional.empty();
-  }
 
-	private static String prettyName(String name) {
-		return StringUtils
-			.capitalize(StringUtils.join(StringUtils.splitByCharacterTypeCamelCase(name), StringUtils.SPACE));
-	}
+    @NotNull
+    private AnAction getAction(Method method, Object object) {
+        return new AnAction(prettyName(method.getName())) {
 
-	private static Predicate<Method> categoryFilter() {
-		return subcategoryFilter()
-			.and(method -> !excludedClasses.contains(method.getReturnType().getSimpleName()));
-	}
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent event) {
+                invokeMethod(method, object, event).ifPresent(fake -> insertFake(event, fake));
+            }
 
-	private static Predicate<Method> subcategoryFilter() {
-		return method -> method.getParameters().length == 0
-			&& Modifier.isPublic(method.getModifiers());
-	}
+            @Override
+            public void update(@NotNull final AnActionEvent event) {
+                final Project project = event.getProject();
+                final Editor editor = event.getData(CommonDataKeys.EDITOR);
+
+                event.getPresentation().setEnabledAndVisible(project != null && editor != null);
+            }
+        };
+    }
+
+    private void insertFake(AnActionEvent event, Object fake) {
+        String text = String.valueOf(fake);
+        final Editor editor = event.getRequiredData(CommonDataKeys.EDITOR);
+        final Project project = event.getRequiredData(CommonDataKeys.PROJECT);
+        final Document document = editor.getDocument();
+
+        Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
+        int start = primaryCaret.getSelectionStart();
+        int end = primaryCaret.getSelectionEnd();
+
+        WriteCommandAction.runWriteCommandAction(project, () -> document.replaceString(start, end, text));
+        primaryCaret.moveToOffset(start + text.length());
+        primaryCaret.removeSelection();
+    }
+
+    private static Optional<Object> invokeMethod(Method method, Object object, AnActionEvent event) {
+        if (method == null || object == null) {
+            return Optional.empty();
+        }
+        final Project project = event.getRequiredData(CommonDataKeys.PROJECT);
+        try {
+            return Optional.ofNullable(method.invoke(object));
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            String msg = String.format("Unable to invoke method '%s()' on object '%s'",
+                    method.getName(), object.getClass().getSimpleName());
+            log.error(msg);
+            FakerNotifier.error(project, msg);
+        }
+        return Optional.empty();
+    }
+
+    private static String prettyName(String name) {
+        return StringUtils
+                .capitalize(StringUtils.join(StringUtils.splitByCharacterTypeCamelCase(name), StringUtils.SPACE));
+    }
+
+    private static Predicate<Method> categoryFilter() {
+        return subcategoryFilter()
+                .and(method -> !excludedClasses.contains(method.getReturnType().getSimpleName()));
+    }
+
+    private static Predicate<Method> subcategoryFilter() {
+        return method -> method.getParameters().length == 0 &&
+                Modifier.isPublic(method.getModifiers()) &&
+                !excludedMethods.contains(method.getName());
+    }
 
 }
